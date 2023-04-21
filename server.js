3a3637fc22fa4ac574f896e00837c53e5aaa8657
PrashantGyawali <49340437+PrashantGyawali@users.noreply.github.com>
2023-04-22 01:08:58 +0545

    Worse version

--- server.js
+++ server.js
@@ -1,32 +1,31 @@
 const http=require("http");
 const fs=require("fs");
 
-//reading html 
-const homepage=fs.readFileSync("./home.html");
-const contactpage=fs.readFileSync("./contact.html")
-
 const PORT = process.env.PORT || 5000; //port number on browser
 
 
-const server= http.createServer(
-    (req,res)=>
-        {
-            if(req.url=="/")
-            {
-              return res.end(homepage);
-            }
-            if(req.url=="/contact"){
-              return  res.end(contactpage);
-            }
-            if(req.url=="/about"){
-              return  res.end("<div>This is about page</div><a href= '/'><button><h2>Home</h2></button></a>");
-            }
-            else{
-              return  res.end("<div>404 not found</div><a href= '/'><button><h2>Home</h2></button></a>")
-            }      
-        }
-);
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+      const filePath = path.join(__dirname, 'output/server/pages', 'home.html');
+      const stream = fs.createReadStream(filePath);
+      return stream.pipe(res);
+    }
+    if (req.url === '/contact') {
+        const filePath = path.join(__dirname, 'output/server/pages', 'contact.html');
+        const stream = fs.createReadStream(filePath);
+        return stream.pipe(res);
+      } 
+      if(req.url=="/about"){
+        return  res.end("<div>This is about page</div><a href= '/'><button><h2>Home</h2></button></a>");
+      }
+      else{
+        return  res.end("<div>404 not found</div><a href= '/'><button><h2>Home</h2></button></a>")
+      }      
 
-server.listen(PORT,()=>{console.log(`server started`)});
+  });
 
-module.exports = server;
+
+  server.listen(PORT,()=>{console.log(`server started`)});
+
+  module.exports = server;
+
